69f710f5a786bcb6c249e7d6f41ef38a812b9eff
Romain MOREL <rmorel@e3r10p13.42.fr>
2021-12-01 19:05:06 +0100

    1er test, ca n'imprime rien

--- Getnextline/get_next_line.c
+++ Getnextline/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 14:27:40 by rmorel            #+#    #+#             */
-/*   Updated: 2021/12/01 15:38:35 by rmorel           ###   ########.fr       */
+/*   Updated: 2021/12/01 19:04:39 by rmorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,87 @@
 
 char	*get_next_line(int fd)
 {
-	static char *line;
-	char		*buff[BUFF_SIZE + 1];
-	int			ret;
+	static int	start_index;
+	char		*str;
 
-	ret = read(fd, buff, BUFF_SIZE);
+	str = ft_newline_to_str(fd, start_index);
+	ft_putstr(str);
+	start_index = start_index + ft_get_size(fd, start_index); 
+	return (str);
 }
 
-int	ft_get_size(int fd)
+void	ft_putstr(char *str)
 {
-	char	*buff[BUFF_SIZE + 1];
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+char	*ft_newline_to_str(int fd, int start_index)
+{
+	char	*s;
+	char	*skip[start_index];
+	int		ret;
+
+	ret = read(fd, skip, start_index);
+	s = malloc(sizeof(*s) * (ft_get_size(fd, start_index) + 1));
+	if (s)
+	{
+		ret = read (fd, s, ft_get_size(fd, start_index));
+		s[ret] = '\0';
+		return (s);
+	}
+	return (NULL);
+}
+
+int	ft_get_size(int fd, int start_index)
+{
+	char	*buff[BUFFER_SIZE];
+	char	*skip[start_index];
 	int		ret;
 	int		size;
 
-	buff = NULL;
 	ret = 0;
 	size = 0;
+	ret = read(fd, skip, start_index);
 	while (1)
 	{
-		ret = read(fd, buff, BUFF_SIZE);
-		if (ret > 0)
+		ret = read(fd, buff, BUFFER_SIZE);
+		if (ret > 0 && ft_get_newline(buff) == BUFFER_SIZE)
 			size = size + ret;
+		else if (ret > 0)
+		{
+			size = size + ft_get_newline(buff);
+			break ;
+		}
 		else
-			break;
+			break ;
 	}
 	return (size);
 }
 
-int	ft_count_char_jump_line
-
-int	main(arg c, arg v)
+int	ft_get_newline(char *str[BUFFER_SIZE])
 {
+	int	i;
 
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] && i != '\n' && i < BUFFER_SIZE)
+		i++;
+	return (i);
+}
 
+int	main()
+{
+	int	fd;
+	
+	fd = open("files/test0", O_RDONLY);
+	get_next_line(fd);
 	return (0);
 }
--- Getnextline/get_next_line.h
+++ Getnextline/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rmorel <rmorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 14:29:30 by rmorel            #+#    #+#             */
-/*   Updated: 2021/12/01 15:21:30 by rmorel           ###   ########.fr       */
+/*   Updated: 2021/12/01 19:04:01 by rmorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 # define	GET_NEXT_LINE_H
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd);
+void	ft_putstr(char *str);
+char	*ft_newline_to_str(int fd, int start_index);
+int		ft_get_size(int fd, int start_index);
+int		ft_get_newline(char *str[BUFFER_SIZE]);
 
 #endif
